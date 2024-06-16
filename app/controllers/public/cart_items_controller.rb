dca2188689dcb35ad48743118c62722716644ccd
HideTsutsumi <167219646+QtCpiqtq@users.noreply.github.com>
2024-06-16 18:01:00 +0000

    [fix]カート内商品の重複の解消

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,27 +3,39 @@ class Public::CartItemsController < ApplicationController
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
-    @cart_item.save
+    in_item = current_customer.cart_items.find_by(item_id: @cart_item.item_id)
+    if in_item.present?
+      in_item.amount += @cart_item.amount.to_i
+      in_item.update(amount: in_item.amount)
+    else
+      @cart_item.save
+    end
     redirect_to cart_items_path
   end
-  
+
   def index
     @cart_items = current_customer.cart_items.all
     @total = 0
   end
-  
+
+  def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_item_params)
+    redirect_to cart_items_path
+  end
+
   def destroy
     cart_item = CartItem.find(params[:id])
     cart_item.destroy
     redirect_to cart_items_path
   end
-  
+
   def destroy_all
     current_customer.cart_items.destroy_all
     redirect_to cart_items_path
   end
-    
-  
+
+
   private
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
